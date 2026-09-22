bb0d215a3ac1ab127366e905b09d11e3ff70168b
theballa666 <302768869+theballa666@users.noreply.github.com>
2026-09-22 13:22:49 -0400

    Cool hover icons and CSS dropdown menu

--- js/comic_header.js
+++ js/comic_header.js
@@ -6,23 +6,90 @@ document.querySelector(".writeHeader").innerHTML = `
         <a href="index.html"><img src="./img/logo.gif" alt="" /></a> 
 
         <div id="nav">
-            <img src="./img/greenseal.png" alt="A green seal" style="width:30px;height:30px;"/>
+            <a href="index.html"><img src="./img/greenseal.png" alt="A green seal" class="nav-seal" style="width:30px;height:30px;"/></a>
             <a href="index.html">HOME</a> |
-            <a href="archive.html">ARCHIVE</a> |
-            <a href="characters.html">CHARACTERS</a> |
+            
+            <div class="dropdown">
+                <span class="dropbtn">COMICS ▾</span>
+                <div class="dropdown-content">
+                    <div class="sub-dropdown">
+                        <a href="archive.html">Ov Flask and Folly ▸</a>
+                        <div class="sub-dropdown-content">
+                            <a href="archive.html">Archive</a>
+                            <a href="characters.html">Characters</a>
+                            <a href="wip.html">WiPs.</a>
+                        </div>
+                        <a href="archive.html">Untitled Metal Comic ▸</a>
+                        <div class="sub-dropdown-content">
+                            <a href="skully.html">Archive</a>
+                            <a href="skully.html">Characters</a>
+                            <a href="skully.html">WiPs.</a>
+                        </div>
+                    </div>
+                </div>
+            </div> |
+
             <a href="about.html">ABOUT</a> |
-            <a href="wip.html">WiPs.</a> |
             <a href="blog.html">BLOG</a> |
             <a href="misc.html">MISC.</a>
         </div>
 
         <div class="disclaimer">
-           <p>Disclaimer: This site is intended primarily for desktop users with marginal support for mobile devices. Get on the computer, now!</p>
-           <p>🏗️The site is also undergoing frequent changes.🏗️</p>
-           <small>While not explicitly an adult site, if you are under the age of 18, you should be doing something else. Like killing people in Grand Theft Auto.</small>
+           <p>Disclaimer: This site is designed for desktop. Get on the computer, now!</p>
+           <p>🏗️ Currently under construction. 🏗️</p>
+           <small>While this is not explicitly an adult site, if you are under the age of 18, you should be doing something else. Like killing people in Grand Theft Auto.</small>
         </div>
 
-<hr>
-
+        <hr>
     </header>
 `;
+
+// Global Header Character Icon Script
+window.addEventListener('DOMContentLoaded', () => {
+    const titleRow = document.querySelector('.archiveTitleRow');
+    if (!titleRow) return;
+
+    // Inject the left and right icon anchor tags if they don't already exist
+    if (!document.getElementById('linkIconLeft')) {
+        const h1 = titleRow.querySelector('h1');
+        if (h1) {
+            const leftAnchor = document.createElement('a');
+            leftAnchor.id = 'linkIconLeft';
+            leftAnchor.href = 'characters.html';
+            leftAnchor.innerHTML = '<img id="titleIconLeft" class="title-icon" alt="Character icon" style="width:50px;height:50px;"/>';
+            
+            const rightAnchor = document.createElement('a');
+            rightAnchor.id = 'linkIconRight';
+            rightAnchor.href = 'characters.html';
+            rightAnchor.innerHTML = '<img id="titleIconRight" class="title-icon" alt="Character icon" style="width:50px;height:50px;"/>';
+
+            titleRow.insertBefore(leftAnchor, h1);
+            titleRow.appendChild(rightAnchor);
+        }
+    }
+
+    const iconPool = [
+        { name: "christoph", normal: "./img/christophfavicon.png", hover: "./img/christoph_shockedfavicon.png", alt: "Christoph icon" },
+        { name: "opal", normal: "./img/opalfavicon.png", hover: "./img/opalsmilefavicon.png", alt: "Opal icon" },
+        { name: "faolan", normal: "./img/faolanfavicon.png", hover: "./img/faolan_winkfavicon.png", alt: "Faolán icon" }
+    ];
+
+    function applyIcon(imgId, linkId, character) {
+        const img = document.getElementById(imgId);
+        const link = document.getElementById(linkId);
+        if (!img || !link) return;
+
+        img.src = character.normal;
+        img.alt = character.alt;
+        img.onmouseover = () => { img.src = character.hover; };
+        img.onmouseout = () => { img.src = character.normal; };
+        
+        // If we are on characters.html, link to anchor. Otherwise, link to characters.html#[name]
+        const isCharsPage = window.location.pathname.includes('characters.html');
+        link.href = isCharsPage ? `#${character.name}` : `characters.html#${character.name}`;
+    }
+
+    const shuffled = [...iconPool].sort(() => Math.random() - 0.5);
+    applyIcon("titleIconLeft", "linkIconLeft", shuffled[0]);
+    applyIcon("titleIconRight", "linkIconRight", shuffled[1]);
+});
